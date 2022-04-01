6973ae44ec6ce21eba3f6b27dc046d260a3cdb52
mIhoom <mdmichalski.poczta@gmail.com>
2022-04-01 15:43:54 +0200

    update

--- Alarm-test.py
+++ Alarm-test.py
@@ -1,37 +1,35 @@
+from sys import breakpointhook
 import tkinter
 from tkinter import *
 import time
 import datetime
 import winsound
 
-def wyswietlanie():
-    godz1 =''
-    obecna = time.strftime('%H:%M:%S')
-    if obecna != godz1:
-            godz1 = obecna
-            l2.config(text = obecna)
-    l2.after(200, wyswietlanie)
-
-def alarm():
-    godzina_alarmu = f"{godz.get()}:{min.get()}:{sek.get()}"
-    print(godzina_alarmu)
-    if (godzina_alarmu != "::"):
-        budzik(godzina_alarmu)
-
-def budzik(godzina_alarmu):
+def alarm(ustaw_czas_alarmu):
     while True:
         time.sleep(1)
-        czas = datetime.datetime.now().strftime('%H:%M%S')
-        print(czas)
-        if czas == godzina_alarmu:
-            pobudka = tkinter.Label(root, text = "Już czas!!!")
-        pobudka.place(x = 83, y = 30)
-        break
+        czas = datetime.datetime.now()
+        aktualny_czas = czas.strftime("%H:%M:%S")
+        print('Aktualny czas: ' + str (aktualny_czas), 'Czas alarmu: ', ustaw_czas_alarmu)
+        if aktualny_czas == ustaw_czas_alarmu:
+            winsound.PlaySound("Music.wav",winsound.SND_ASYNC)
+            print("Udało się!!!")
+            break
+ 
+def pobierz_czas_alarmu():
+    ustaw = f"{godz.get()}:{min.get()}:{sek.get()}"
+    alarm(ustaw)
 
 
+def wyswietlanie():
+    obecna = datetime.datetime.now().strftime("%H:%M:%S")
+    l2.config(text = obecna)
+    l2.after(200, wyswietlanie)
+        
 root = tkinter.Tk()
 root.title("Alarm Michała")
 root.geometry('340x200')
+root.resizable(width=False,height=False)
 
 godz = StringVar()
 min = StringVar()
@@ -40,21 +38,21 @@ sek = StringVar()
 l = tkinter.Label(root, text = 'Aktualna godzina: ', font = ('Calibri', 12), width = '20')
 l.pack()
 l2 = tkinter.Label(root, text = wyswietlanie, font=('Calibri', 25) ,bg = 'black', fg = 'green', height = '1', width = '10')
-l2.place(x = 83, y = 30)
+l2.place(x = 80, y = 25)
 l3 = tkinter.Label(root, text = 'Godzina alarmu: \n godz. min. sek.', font = ('Calibri', 8), width = '12')
-l3.place(x = 10 , y = 96)
+l3.place(x = 10 , y = 81)
+l4 = tkinter.Label(root, text = 'Podaj wszystkie wartości (format 24 godz.)', font = ('Calibri', 8), width = '40')
+l4.place(x = 48, y = 115)
 
-b = tkinter.Button(root, text = 'ustaw alarm', command = budzik, font = ('Calibri', 16), bg = 'grey', fg = 'white')
+egodziny = tkinter.Entry(root, textvariable = godz, bg = 'white', fg = 'black', font = 15, width = 4)
+egodziny.place(x = 97, y = 85) 
+eminuty = tkinter.Entry(root, textvariable = min, bg = 'white', fg = 'black', font = 15, width = 4)
+eminuty.place(x = 147, y = 85)
+esekundy = tkinter.Entry(root, textvariable = sek, bg = 'white', fg = 'black', font = 15, width = 4)
+esekundy.place(x = 197, y = 85)
+
+b = tkinter.Button(root, command = pobierz_czas_alarmu, text = 'Utaw alarm', font = ('Calibri', 16), bg = 'grey', fg = 'white')
 b.place(x = 110, y = 140)
- 
-egodziny = tkinter.Entry(root, bg = 'white', fg = 'black', font = 15, width = 4)
-egodziny.place(x = 97, y = 100,) 
-eminuty = tkinter.Entry(root, bg = 'white', fg = 'black', font = 15, width = 4)
-eminuty.place(x = 147, y = 100)
-esekundy = tkinter.Entry(root, bg = 'white', fg = 'black', font = 15, width = 4)
-esekundy.place(x = 197, y = 100)
 
 wyswietlanie()
-alarm()
-budzik()
 root.mainloop()
--- Alarm.py
+++ Alarm.py
@@ -4,28 +4,30 @@ import time
 import datetime
 import winsound
 
-def wyswietlanie():
-    godz1 =''
-    obecna = time.strftime('%H:%M:%S')
-    if obecna != godz1:
-            godz1 = obecna
-            l2.config(text = obecna)
-    l2.after(200, wyswietlanie)
-
-def alarm():
+def alarm(ustaw_czas_alarmu):
     while True:
-        ustaw_czas_alarmu = f"{godz.get()}:{min.get()}:{sek.get()}"
         time.sleep(1)
-        obecny_czas = datetime.datetime.now().strftime("%H:%M:%S")
-        print("Aktualny czas: ", obecny_czas, "Czas alarmu: ", ustaw_czas_alarmu)
-        if obecny_czas == ustaw_czas_alarmu:
-            print("JUŻ CZAS!!!")
-        winsound.PlaySound("sound.wav",winsound.SND_ASYNC)
-        break
+        czas = datetime.datetime.now()
+        aktualny_czas = czas.strftime("%H:%M:%S")
+        print('Aktualny czas: ' + str (aktualny_czas), "," 'Czas alarmu: ', ustaw_czas_alarmu)
+        if aktualny_czas == ustaw_czas_alarmu:
+            winsound.PlaySound("Music.wav",winsound.SND_ASYNC)
+            print("Alarm!!!")
+            break
+ 
+def pobierz_czas_alarmu():
+    ustaw = f"{godz.get()}:{min.get()}:{sek.get()}"
+    alarm(ustaw)
 
+def wyswietlanie():
+    obecna = datetime.datetime.now().strftime("%H:%M:%S")
+    l2.config(text = obecna)
+    l2.after(200, wyswietlanie)
+        
 root = tkinter.Tk()
 root.title("Alarm Michała")
 root.geometry('340x200')
+root.resizable(width=False,height=False)
 
 godz = StringVar()
 min = StringVar()
@@ -34,20 +36,21 @@ sek = StringVar()
 l = tkinter.Label(root, text = 'Aktualna godzina: ', font = ('Calibri', 12), width = '20')
 l.pack()
 l2 = tkinter.Label(root, text = wyswietlanie, font=('Calibri', 25) ,bg = 'black', fg = 'green', height = '1', width = '10')
-l2.place(x = 83, y = 30)
+l2.place(x = 80, y = 25)
 l3 = tkinter.Label(root, text = 'Godzina alarmu: \n godz. min. sek.', font = ('Calibri', 8), width = '12')
-l3.place(x = 10 , y = 96)
+l3.place(x = 10 , y = 81)
+l4 = tkinter.Label(root, text = 'Podaj wszystkie wartości (format 24 godz.)', font = ('Calibri', 8), width = '40')
+l4.place(x = 48, y = 115)
 
 egodziny = tkinter.Entry(root, textvariable = godz, bg = 'white', fg = 'black', font = 15, width = 4)
-egodziny.place(x = 97, y = 100) 
+egodziny.place(x = 97, y = 85) 
 eminuty = tkinter.Entry(root, textvariable = min, bg = 'white', fg = 'black', font = 15, width = 4)
-eminuty.place(x = 147, y = 100)
+eminuty.place(x = 147, y = 85)
 esekundy = tkinter.Entry(root, textvariable = sek, bg = 'white', fg = 'black', font = 15, width = 4)
-esekundy.place(x = 197, y = 100)
+esekundy.place(x = 197, y = 85)
 
-b = tkinter.Button(root, command = alarm, text = 'ustaw alarm', font = ('Calibri', 16), bg = 'grey', fg = 'white')
+b = tkinter.Button(root, command = pobierz_czas_alarmu, text = 'Utaw alarm', font = ('Calibri', 16), bg = 'grey', fg = 'white')
 b.place(x = 110, y = 140)
 
 wyswietlanie()
-alarm()
 root.mainloop()
